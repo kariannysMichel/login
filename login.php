5804cb4005488430740ff2a87aa31b48a1e4299c
kerry_10 <kariannysmichelpq@gmail.com>
2022-11-28 17:05:21 -0400

    first commit

---
+++ login.php
@@ -0,0 +1,176 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+  header("location: welcome.php");
+  exit;
+}
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Por favor ingrese su usuario.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Por favor ingrese su contraseña.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirect user to welcome page
+                            header("location: welcome.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "La contraseña que has ingresado no es válida.";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = "No existe cuenta registrada con ese nombre de usuario.";
+                }
+            } else{
+                echo "Algo salió mal, por favor vuelve a intentarlo.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+
+    <header>
+    <nav>
+       
+       <img src="WhatsApp Image 2022-10-10 at 5.52.14 PM.jpeg"width="1000px"> 
+            <img src="uni.jpeg" width="100px">
+
+
+
+    
+            <p> <div><h4>Kariannys quijada</h4> </div> 
+        
+
+    
+            <div><h4>ci:30.249.035</h4></div><h4>
+        
+
+        
+            <div><h4>seccion T2-INF-2</h4></div> </p>
+        
+        
+        <img src="pinguino.jpeg" width="100px" >
+
+    </nav>
+
+ </header> 
+<article>
+    
+    <section>
+        <h1> Kariannys es una alumna de la UNIVERSIDAD POLITECNICA TERRITORIAL DEL ESTADO BOLIVAR que esta creando su primera pagina de registro de usuario</h1>
+    </section>
+
+</article>
+    <div class="wrapper">
+
+        <h2>Login</h2>
+
+        <p>Por favor, complete sus credenciales para iniciar sesión.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Usuario</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Contraseña</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Ingresar">
+            </div>
+            <p>¿No tienes una cuenta? <a href="register.php">Regístrate ahora</a>.</p>
+        </form>
+    </div>  
+
+    <aside>
+    <h1>Quieres saber mas sobre Kariannys</h1>
+
+    <div> 
+        <a href="https://www.facebook.com/kariannys.michel/">facebook</a> <img src="índice.png" width="25px">
+    </div>
+
+    <div>
+        <a href="https://www.instagram.com/kariannys_michel/following/">instagram</a> <img src="instagram.jpeg" width="25px"> 
+    </div>
+
+    <div>
+        <a href="https://github.com/kariannysMichel">github</a> <img src="github.png" width="35px">
+    </div>
+
+</aside>  
+</body>
+</html>
